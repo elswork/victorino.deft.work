28a8a010f53f6e9e349e455ff59427420f516a54
Eloy López <elswork@gmail.com>
2024-02-17 19:54:41 +0100

    Añado pequeños cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://victorino.deft.work',
 	integrations: [mdx(), sitemap()],
 });
